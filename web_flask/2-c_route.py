43d8b0afcd56023cd9fe0150dca79ddf9e63da4d
Rayidahub <rayidatech@gmail.com>
2023-05-22 13:15:50 -0400

    execute1

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
-from flask import Flask
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def home_page():
-    """Display str"""
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Display HBNB"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Display C + text"""
+    """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
-    return f"C {text}"
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0")if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
