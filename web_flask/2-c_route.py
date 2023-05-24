aad1e94ea83a6f734299ea950b61344ca64c3363
david <mugomadavid24@gmail.com>
2023-05-24 10:24:46 +0300

    changes made

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
+"""This script starts a Flask web applicatio
+ with two routings on 0.0.0.0, port 5000
 """
-Script that starts a Flask web application
-"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
+    """Return string when route queried
+    """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def holberon():
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
+    """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    return "C {}" .format(text.replace("_", " "))
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
